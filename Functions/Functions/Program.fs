e67b6263bf2e244fc518b9e52567dc036fb1db51
Christa Bridges <christa@pebblepad.co.uk>
2021-02-02 15:55:56 +0000

    master Started function section of tutorial

---
+++ Functions/Functions/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let do_funcs() = 
+    let get_sum (x: int, y: int) : int = x + y
+
+    printf "Enter A Number: "
+    let first_number = Console.ReadLine()
+
+    printf "Enter Another Number: "
+    let second_number = Console.ReadLine()
+
+    printfn "%s + %s = %i" first_number second_number (get_sum(int first_number, int second_number))
+
+do_funcs()
+
+Console.ReadKey() |> ignore
