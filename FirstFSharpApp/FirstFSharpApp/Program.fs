9b63b70b07bb81b74335c8988ec0e1ca50de6875
Christa Bridges <christa@pebblepad.co.uk>
2021-02-02 15:17:03 +0000

    master Completed the inputs and outputs section of the F# tutorial.

--- FirstFSharpApp/FirstFSharpApp/Program.fs
+++ FirstFSharpApp/FirstFSharpApp/Program.fs
@@ -11,8 +11,17 @@ let hello() =
     // Output with string placeholder and new line
     printfn "Hello %s" name
 
+let float_format() = 
+    let pi = 3.141592652589793238462643383
+    let big_pi = 3.141592652589793238462643383M
+    printfn "Pi %.4f" pi
+    printfn "Pi %M" big_pi
+    printfn "%-5s %5s" "a" "b"
+    printfn "%*s" 10 "Hi"
+
 // Calls the function
 hello()
+float_format()
 
 // Keeps the window open
 Console.ReadKey() |> ignore
