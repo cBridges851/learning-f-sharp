49951971b14893d7243303e9c1dda5d57774bea8
Christa Bridges <christa@pebblepad.co.uk>
2021-02-02 15:41:06 +0000

    master Completed the bindings/mutable part of the tutorial. A program has been made in the Bindings And Mutables project to demonstrate the difference between the two.

---
+++ Bindings and Mutables/Bindings and Mutables/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+
+let bind_stuff() = 
+    let mutable weight = 175
+    printfn "Starting Weight: %i" weight
+    weight <- 170
+    printfn "New Weight: %i" weight
+
+    let a = ref 10
+    let b = a
+    b := 15
+    // They will both be 15 because it is changing a value a and b reference
+    printfn "This is the value of a: %i" ! a
+    printfn "This is the value of b: %i" ! b
+
+    let mutable c = 10
+    let mutable d = c
+    d <- 15
+    // c will be 10 and d will be 15 because they are created as variables on the stack, so are separate from each other
+    printfn "This is the value of c: %i" c
+    printfn "This is the value of d: %i" d
+
+
+bind_stuff()
+Console.ReadKey() |> ignore
