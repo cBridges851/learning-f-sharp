37f13411425e6c8a8435b64b0b3e322f5dc53725
Christa Bridges <christa@pebblepad.co.uk>
2021-02-23 16:45:06 +0000

    master Completed the parts of the tutorial relating to looping. This included while loops, for loops and other ways of interating.

---
+++ Looping/Looping/Program.fs
@@ -0,0 +1,41 @@
+﻿open System
+
+let while_loops() = 
+    // Number guessing game
+    let magic_num = "7"
+    let mutable guess = ""
+
+    while not (magic_num.Equals(guess)) do 
+        printf "Guess the number I'm thinking of: "
+        guess <- Console.ReadLine()
+
+    printfn "Congratulations! You have guessed the number!"
+
+let for_loops()=
+    // Going up
+    for i = 0 to 10 do
+        printfn "%i" i
+
+    printfn "" 
+
+    // Going down
+    for i = 10 downto 0 do
+        printfn "%i" i
+
+    printfn ""
+
+    // In a range
+    for i in [1..10] do
+        printfn "%i" i
+
+    // Pipeline
+    [1..10] |> List.iter(printfn "Num: %i")
+
+    // Get sum
+    let sum = List.reduce (+) [1..10]
+    printfn "Sum: %i" sum
+
+for_loops()
+while_loops()
+
+Console.ReadKey() |> ignore
