472fa3f316f480d9f82cb139c586696f35c67bff
Christa Bridges <christa@pebblepad.co.uk>
2021-03-09 16:42:05 +0000

    master Completed the tuples section of the tutorial.

---
+++ Tuple/Tuple/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let tuples() =
+    printfn "Tuples"
+
+    printfn "First, let's calculate averages!"
+    let avg (a, b, c, d) : float = 
+        let sum = a + b + c + d
+        sum / 4.0
+
+    printfn "Average: %f" (avg (1.0, 2.0, 3.0, 4.0))
+
+    let data = ("Christa", 19, 3.14)
+    let (name, age, _) = data
+
+    printfn "%s" name
+
+tuples()
+
+Console.ReadKey() |> ignore
+
