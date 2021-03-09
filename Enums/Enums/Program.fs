36f8c482ccf2b797c31555122ff55a85cc6eb0af
Christa Bridges <christa@pebblepad.co.uk>
2021-03-09 15:58:06 +0000

    master The enums section of the tutorial has been completed.

---
+++ Enums/Enums/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+
+type programming_language = 
+| c_sharp = 0
+| javascript = 1
+| python = 2
+
+let enums() =
+    let users_language = programming_language.python
+
+    match users_language with
+    | programming_language.c_sharp -> printfn "C# is your programming language!" 
+    | programming_language.javascript -> printfn "JavaScript is your programming language!" 
+    | programming_language.python -> printfn "Python is your programming language!" 
+    | _ -> printfn "The selected programming language is not C#, JavaScript, or Python"
+    
+enums()
+
+Console.ReadKey() |> ignore
