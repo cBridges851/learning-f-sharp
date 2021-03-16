167035e62f675fff724cf813607b05777b020653
Christa Bridges <christa@pebblepad.co.uk>
2021-03-16 16:02:37 +0000

    master Completed the records section of the tutorial.

---
+++ Records/Records/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+type Cat = {
+    Name: string;
+    Age: int
+}
+
+let records() = 
+    printfn "This is the records function"
+    let newCat = {
+        Name = "Bob";
+        Age = 5
+    }
+
+    printfn "This is my new cat! Its name is %s and they are %i years old" newCat.Name newCat.Age
+
+records()
+
+Console.ReadKey() |> ignore
