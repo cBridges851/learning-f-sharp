6133a24e200b6112ceea3618b8a0c3e037f380f0
Christa Bridges <christa@pebblepad.co.uk>
2021-03-23 16:20:35 +0000

    master Completed exception handlin section of the tutorial.

---
+++ Exceptions/Exceptions/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let exception_example x y = 
+    printfn "Exceptions"
+    printfn "%i %i" x y
+
+    try
+        printfn "%i / %i = %A" x y (x/y)
+    with 
+        | :? System.DivideByZeroException as ex -> printfn "Cannot divide by 0!"
+        
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    exception_example 4 0
+    0 // return an integer exit code
