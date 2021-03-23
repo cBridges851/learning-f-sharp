178a8ddc5d15dd80b5afaa78fed60d00fb4f87fd
Christa Bridges <christa@pebblepad.co.uk>
2021-03-23 16:54:16 +0000

    master Completed the structs section of the tutorial.

---
+++ Structs/Structs/Program.fs
@@ -0,0 +1,24 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+type Rectangle = struct
+    val Length: float
+    val Width: float 
+
+    new (length, width) = 
+        {Length = length; Width = width}
+end
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let area(shape: Rectangle) = 
+        shape.Length * shape.Width
+
+    let new_rectangle = new Rectangle(5.0, 6.0)
+
+    let rect_area = area new_rectangle
+    printfn "%A" new_rectangle.Length
+    printfn "Area: %.2f" rect_area
+    0 // return an integer exit code
